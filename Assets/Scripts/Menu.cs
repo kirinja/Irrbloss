48bc4589549d56a679fd991c78eeb2d1567eb20d
Niklas Blom <blom.niklas@gmail.com>
2017-10-19 11:41:03 +0200

    Fixade menu (gör om iteration 3)

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FinishedLevel");
+    }
+
+    public void LoadInstruction()
+    {
+        SceneManager.LoadScene("Instruktion");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
